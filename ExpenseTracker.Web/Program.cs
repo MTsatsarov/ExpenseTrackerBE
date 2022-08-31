3b5db991f9ff159a57927a4cfa5625384ef994e4
Mihail Tsatsarov <77450034+MTsatsarov@users.noreply.github.com>
2022-08-31 21:48:24 +0300

    DBInit2

--- ExpenseTracker.Web/Program.cs
+++ ExpenseTracker.Web/Program.cs
@@ -1,6 +1,53 @@
+using ExpenseTracker.Data;
+using ExpenseTracker.Data.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ExpenseTrackerDbContext>(opt =>
+opt.UseSqlServer("name=ConnectionStrings:DefaultConnection").UseLazyLoadingProxies());
+
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>()
+	.AddEntityFrameworkStores<ExpenseTrackerDbContext>()
+	.AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(x =>
+{
+	x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+	x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(o =>
+{
+	var Key = Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]);
+	o.SaveToken = true;
+	o.TokenValidationParameters = new TokenValidationParameters
+	{
+		ValidateIssuer = false,
+		ValidateAudience = false,
+		ValidateLifetime = true,
+		ValidateIssuerSigningKey = true,
+		ValidIssuer = builder.Configuration["JWT:Issuer"],
+		ValidAudience = builder.Configuration["JWT:Audience"],
+		IssuerSigningKey = new SymmetricSecurityKey(Key)
+	};
+});
+
+builder.Services.AddCors(c =>
+{
+	c.AddPolicy(name: "AllowOrigins",
+	 options =>
+	 {
+		 options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+	 });
+
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -18,6 +65,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
