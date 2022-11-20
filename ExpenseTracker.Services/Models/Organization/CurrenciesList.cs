eb52820348f93c926d081df9d3552879d1d86202
Mihail Tsatsarov <77450034+MTsatsarov@users.noreply.github.com>
2022-11-21 00:19:34 +0200

    Currency

---
+++ ExpenseTracker.Services/Models/Organization/CurrenciesList.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ExpenseTracker.Services.Models.Organization
+{
+	public class CurrenciesList
+	{
+		[JsonPropertyName("currency")]
+		public string Currency { get; set; }
+
+		[JsonPropertyName("abbreviation")]
+		public string Abbreviation { get; set; }
+
+		[JsonPropertyName("symbol")]
+		public string Symbol { get; set; }
+	}
+}
