6fe5c30d2f2c1713858e0f40846aa85e196cdec1
Mihail Tsatsarov <77450034+MTsatsarov@users.noreply.github.com>
2022-11-30 14:33:48 +0200

    Seed admin user

---
+++ ExpenseTracker.Data/Seeding/Seeders/ApplicationUserSeeder.cs
@@ -0,0 +1,42 @@
+﻿using ExpenseTracker.Common;
+using ExpenseTracker.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExpenseTracker.Data.Seeding.Seeders
+{
+	public class ApplicationUserSeeder : ISeeder
+	{
+		public async Task SeedAsync(ExpenseTrackerDbContext dbContext, IServiceProvider serviceProvider)
+		{
+			var roleManager = serviceProvider.GetRequiredService<RoleManager<ApplicationRole>>();
+			var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+			var password = "AaBbCc1!";
+			var user = new ApplicationUser()
+			{
+				UserName = "Admin",
+				Email = "Admin@admin.bg",
+				FirstName = "Admin",
+				LastName = "Admin",
+				Organization = new Organization()
+				{
+					Name = "Admin organization",
+					Currency = "Bulgaria Lev",
+					Abbreviation = "BGN",
+					CurrencySymbol = "лв"
+				},
+				Settings = new Settings()
+				{
+					Mode = "dark"
+				}
+
+			};
+
+			await userManager.CreateAsync(user, password);
+
+
+			await userManager.AddToRoleAsync(user, RoleConstants.Admin);
+		}
+	}
+}
